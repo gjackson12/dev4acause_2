7db69be700dc1c3bbdd4060acce0c709d278c034
Graham Jackson <graham.jackson07@gmail.com>
2013-09-28 20:19:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dev4acause::Application.config.secret_key_base = '2919cebc2a3930723fd18b89f34d00eef8d7ee71953d8aa1a9bb87e703d46c87d38a96446fd1bca54e879bead03094a6b754d409ba8950c25892edbb6d45084b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dev4acause::Application.config.session_store :cookie_store, key: '_dev4acause_session'
