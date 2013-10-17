362bec3fc5e295ae64ab5419559c087b5c880b20
Graham Jackson <graham.jackson07@gmail.com>
2013-10-17 14:54:42 -0400

    added more ui improvements

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,15 @@
-# CarrierWave.configure do |config|
+CarrierWave.configure do |config|
 
-#   if Rails.env.development? || Rails.env.test?
-#     config.storage = :file
-#   else
-#     config.storage = :fog
-#     config.fog_credentials = {
-#       :provider               => 'AWS',
-#       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-#       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-#       :region                 => 'Oregon'
-#     }
-#     config.fog_directory = ENV['FOG_DIRECTORY']
-#   end
-# end
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => 'oregon'
+    }
+    config.fog_directory = ENV['FOG_DIRECTORY']
+  end
+end
